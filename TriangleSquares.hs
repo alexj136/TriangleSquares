0101bc7759807bb480b3d1c0809b3e958a50d678
Alex Jeffery <apjeffery136@googlemail.com>
2016-07-14 14:06:18 +0300

    Made things more readable

--- TriangleSquares.hs
+++ TriangleSquares.hs
@@ -4,10 +4,40 @@ main :: IO ()
 main = putStr
     . concat
     . map printOne
-    $ computeTrackingSeeds ( zip triangles [ 1 .. ] ) ( zip squares [ 1 .. ] ) 1
+    . index
+    $ commonElementsWithIndexes ( index triangles ) ( index squares )
+
+-----------
+-- TYPES --
+-----------
+
+-- These make things a bit more readable
+newtype Index = Index Integer deriving (Show, Eq, Ord)
+type Indexed a = (Index, a)
+type IndexedList a = [Indexed a]
+
+------------------------------------------------
+-- FUNCTIONS FOR MANIPULATING DATA STRUCTURES --
+------------------------------------------------
+
+-- Label all elements of a list with their index
+index :: [a] -> IndexedList a
+index = zip ( map Index [ 0 .. ] )
+
+-- Given an indexed element, get its index
+indexOf :: Indexed a -> Index
+indexOf (idx, _) = idx
+
+-- Given an indexed element, get its value
+valueOf :: Indexed a -> a
+valueOf (_, a) = a
 
 listAllPositive :: (Integer -> Integer) -> [Integer]
-listAllPositive f = map f [ 1 .. ]
+listAllPositive f = map f [ 0 .. ]
+
+---------------------------
+-- THE INTERESTING STUFF --
+---------------------------
 
 -- Calculate the nth square number
 square :: Integer -> Integer
@@ -19,28 +49,25 @@ squares = listAllPositive square
 
 -- Calculate the nth triangle number
 triangle :: Integer -> Integer
-triangle 0         = 0
-triangle n | n < 0 = n + triangle (n + 1)
-triangle n | n > 0 = n + triangle (n - 1)
+triangle n = (n * (n - 1)) `div` 2
 
--- A list of all the triangle numbers
+-- A list of all the triangle numbers, Start
 triangles :: [Integer]
-triangles = listAllPositive triangle
+triangles = tail $ listAllPositive triangle
 
 -- Explain a tuple with index, triangle-square number, the triangle root and
 -- the square root
-printOne :: (Integer, Integer, Integer, Integer) -> String
-printOne (i, n, t, s) = show i ++ ": " ++ show n ++ " is the " ++ show t ++
-    "th triangle and " ++ show s ++ " squared\n"
+printOne :: Indexed (Integer, Index, Index) -> String
+printOne (Index i, (n, Index t, Index s)) = concat [ show i, ": ", show n,
+    " is the ", show t, "th triangle and ", show s, " squared\n" ]
 
--- Main computation
-computeTrackingSeeds :: Ord a =>
-    [(a,a)] -> [(a,a)] -> Integer -> [(Integer,a,a,a)]
-computeTrackingSeeds []     _      i          = []
-computeTrackingSeeds _      []     i          = []
-computeTrackingSeeds ((a, ga):as) ((b, gb):bs) i | a <  b =
-    computeTrackingSeeds as ((b, gb):bs) i
-computeTrackingSeeds ((a, ga):as) ((b, gb):bs) i | a >  b =
-    computeTrackingSeeds ((a, ga):as) bs i
-computeTrackingSeeds ((a, ga):as) ((b, gb):bs) i | a == b =
-    (i, a, ga, gb) : computeTrackingSeeds as bs (i + 1)
+-- Given two ordered indexed lists, find elements that are found in both lists
+-- and their indices in each list
+commonElementsWithIndexes :: Ord a => IndexedList a -> IndexedList a ->
+    [(a, Index, Index)]
+commonElementsWithIndexes []     _      = []
+commonElementsWithIndexes _      []     = []
+commonElementsWithIndexes (a:as) (b:bs) =
+    if valueOf a < valueOf b then commonElementsWithIndexes as (b:bs) else
+    if valueOf a > valueOf b then commonElementsWithIndexes (a:as) bs else
+    (valueOf a, indexOf a, indexOf b) : commonElementsWithIndexes as bs
